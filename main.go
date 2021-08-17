94c03d6bd992c9f7ff8aafcd9af201a8e1a06133
Ray <xuruicheng96@gmail.com>
2021-08-16 22:12:02 -0500

    add learning note, unfinished

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	pointers_demo()
+	structs_demo()
+	struct_literals()
+	arrays_demo()
+	slices_demo()
+	slice_literals_demo()
+}
+
+func pointers_demo() {
+	// There's no pointer arithmetics in Go
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+}
+
+func structs_demo() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
+
+//############ struct literals ##############
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
+func struct_literals() {
+	fmt.Println(v1, p, v2, v3)
+}
+
+func arrays_demo() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
+
+func slices_demo() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+	/*
+		Slices are like references to arrays
+		A slice does not store any data, it just describes a section of an underlying array.
+		Changing the elements of a slice modifies the corresponding elements of its underlying array.
+		Other slices that share the same underlying array will see those changes.
+
+	*/
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+
+}
+
+/*
+	A slice literal is like an array literal without the length.
+	This is an array literal:
+	[3]bool{true, true, false}
+	And this creates the same array as above, then builds a slice that references it:
+	[]bool{true, true, false}
+*/
+func slice_literals_demo() {
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	//array of struct on the fly
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+}
